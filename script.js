a70cb9419b93685b78280b7e8fd2f89cd30ef058
Mahin-Abrar <mahinabrar25@gmail.com>
2023-11-15 12:07:38 +0600

    joshim Calculator

--- script.js
+++ script.js
@@ -2,28 +2,114 @@
 // $(".button").on("click", function() {
 //     console.log("Button clicked");
 // });
-let string = "";
+
+
+// function deleteLastDigit() {
+//   let input = document.getElementById('display');
+//   let buttons = display.value;
+
+
+//   var newValue = currentValue.slice(0, -1);
+
+
+//   display.value = newValue;
+// }
+
+
+let input = document.querySelector('.input');
 let buttons = document.querySelectorAll('.button');
-console.log
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML =="💩"){
-        string=eval(string);
-        let newString=String(string).slice(0,-1);
-      document.querySelector('input').value = newString;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
+document.addEventListener("DOMContentLoaded",function() { 
+  buttons.forEach(function(button){
+    button.addEventListener('click',function() {
+      let buttonInput =button.innerHTML;
+      switch (buttonInput){
+        case 'C':
+          input.value='';
+          break;
+        case '⌦':
+            input.value=input.value.slice(0,-1);
+            return input;
+            break;
+            case '=':
+              try {
+                input.value = eval(input.value);
+              } catch (error) {
+                input.value = '';
+                let overlay = document.createElement('div');
+                overlay.style.position = 'fixed';
+                overlay.style.top = '0';
+                overlay.style.left = '0';
+                overlay.style.width = '100%';
+                overlay.style.height = '100%';
+                overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.7)'; 
+    
+                let img = document.createElement('img');
+                img.src = 'oops.jpeg';
+                img.alt = 'Opps Image';
+                img.style.width = '100%';
+                img.style.height = '100%';
+                overlay.appendChild(img);
+                document.body.appendChild(overlay);
+                break;
+              }
+        // case '=':
+        //   try {
+        //     input.value = eval(input.value);
+        //   } catch (error) {
+        //     input.value = 'Opps';
+        //   }
+          break;
+          default:
+          input.value += buttonInput;
+          break;
       }
-  })
-})
+    });
+  });
+});
+
+
+      
+        
+
+
+
+// function deleteLastDigit() {
+//   var display = document.getElementById('display');
+//   var currentValue = display.value;
+
+
+//   var newValue = currentValue.slice(0, -1);
+
+
+//   display.value = newValue;
+// }
+
+// let input = document.querySelector('.input');
+//   let buttons = document.querySelectorAll('.button');
+// document.addEventListener("DOMContentLoaded", function () {
+//
+
+//   buttons.forEach(function (button) {
+//     button.addEventListener('click', function () {
+//       let buttonInput = button.textContent;
+
+//       if (buttonInput === 'C') {
+//         // Clear the input field
+//         input.value = '';
+//       } else if (buttonInput === '⌦') {
+//         // Backspace functionality
+//         input.value = input.value.slice(0, -1);
+//       } else if (buttonInput === '=') {
+//         // Evaluate the expression
+//         try {
+//           input.value = eval(input.value);
+//         } catch (error) {
+//           input.value = 'Error';
+//         }
+//       } else {
+//         // Append the button value to the input field
+//         input.value += buttonInput;
+//       }
+//     });
+//   });
+// });
